ec410c1ff2ad9eb39e4b6e67dc3db7bdb981d75c
ViacheslavSavitskyiUA <viacheslav.savitskyi74@gmail.com>
2023-09-28 17:09:13 +0300

    init clear project

---
+++ ScheduleSUICT/ScheduleSUICT/ScheduleSUICTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleSUICTApp.swift
+//  ScheduleSUICT
+//
+//  Created by Viacheslav Savitskyi on 28.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScheduleSUICTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
